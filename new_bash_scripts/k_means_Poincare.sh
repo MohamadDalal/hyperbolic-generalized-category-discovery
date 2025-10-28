786734ab038c2b9c0ad1b54527c576b5fec3695b
Mohamad Dalal <moda@create.aau.dk>
2025-10-28 14:28:23 +0100

    Created Poincare scripts for extracting features and doing K-Means

---
+++ new_bash_scripts/k_means_Poincare.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# #SBATCH --output="logs/GCD-KMeans-Aircraft-Hyperbolic-Angle-SGD-HCD-DINOv2.log"
+# #SBATCH --job-name="GCD-KMeans-Aircraft-Hyperbolic-Angle-SGD-HCD-DINOv2"
+#SBATCH --time=12:00:00
+#SBATCH --signal=B:SIGTERM@30
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=16G
+# #SBATCH --nodelist=ailab-l4-07
+# #SBATCH --exclude=nv-ai-03
+
+#####################################################################################
+
+data=$1
+c=$2
+cs=$3
+HypCD_mode=$4
+seed=$5
+Euclidean=$6
+
+# Script arguments
+container_path="${HOME}/pytorch25-06.sif"
+
+# Dynamically set output and error filenames using job ID and iteration
+outfile="logs/GCD-KMeans-${data}-Hyperbolic-Poincare-c-${c}-cs-${cs}-HypCD-${HypCD_mode}-Seed${seed}-Euclidean${Euclidean}.out"
+
+# Print the filenames for debugging
+echo "Output file: ${outfile}"
+#echo "Error file: ${errfile}"
+#echo "Restart num: ${restarts}"
+echo "Using container: ${container_path}"
+
+PYTHON='venv/bin/python'
+
+hostname
+nvidia-smi
+
+#export CUDA_VISIBLE_DEVICES=0
+
+# Get unique log file
+#SAVE_DIR=/work/sagar/osr_novel_categories/dev_outputs/
+
+#EXP_NUM=$(ls ${SAVE_DIR} | wc -l)
+#EXP_NUM=$((${EXP_NUM}+1))
+#echo $EXP_NUM
+
+if [ "$Euclidean" = "True" ]; then
+    echo "Clustering in Euclidean space"
+    srun --output="${outfile}" --error="${outfile}" singularity exec --nv ${container_path} ${PYTHON} -m methods.clustering.k_means --dataset ${data} --semi_sup 'True' --use_ssb_splits 'True' \
+    --use_best_model 'False' --max_kmeans_iter 200 --k_means_init 100 --warmup_model_exp_id "Hyperbolic-Poincare-c-${c}-cs-${cs}-HypCD-${HypCD_mode}-Seed${seed}_Euclidean" --hyperbolic 'False' --poincare 'True' --cluster_size ${cs} #--K 50
+else
+    echo "Clustering in Hyperbolic space"
+    srun --output="${outfile}" --error="${outfile}" singularity exec --nv ${container_path} ${PYTHON} -m methods.clustering.k_means --dataset ${data} --semi_sup 'True' --use_ssb_splits 'True' \
+    --use_best_model 'False' --max_kmeans_iter 200 --k_means_init 100 --warmup_model_exp_id "Hyperbolic-Poincare-c-${c}-cs-${cs}-HypCD-${HypCD_mode}-Seed${seed}" --hyperbolic 'True' --poincare 'True' --cluster_size ${cs} #--K 50
+fi
+
+ #> ${SAVE_DIR}logfile_${EXP_NUM}.out
